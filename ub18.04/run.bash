ff2f56aae7aa55453f3613bd89c0b020825d362a
Fernando Cladera <fcladera@fcladera.com>
2022-02-18 09:49:45 -0500

    Updated ub18.04 image

--- ub18.04/run.bash
+++ ub18.04/run.bash
@@ -2,10 +2,11 @@
 
 # based on https://github.com/iandouglas96/jackal_master/blob/master/run.bash
 
-IMG="ub18.04-cudagl-11.3"
+# Set the image name to the current dir
+curr_dir=${PWD##*/}
+IMG="fclad/$curr_dir"
 
 # Make sure processes in the container can connect to the x server
-# Necessary so gazebo can create a context for OpenGL rendering (even headless)
 XAUTH=/tmp/.docker.xauth
 if [ ! -f $XAUTH ]
 then
@@ -19,12 +20,10 @@ then
     chmod a+r $XAUTH
 fi
 
-echo "RUNNING DOCKER IMAGE: " $1
-
 docker run --gpus all \
   -u 1000 \
   -it \
-  --workdir /home/dcist \
+  --workdir /home/fclad \
   --privileged \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
@@ -32,15 +31,15 @@ docker run --gpus all \
   -v "/tmp/.X11-unix:/tmp/.X11-unix" \
   -v "/etc/localtime:/etc/localtime:ro" \
   -v "/dev:/dev" \
-  -v "/media/$USER:/media/dcist" \
+  -v "/media/$USER:/media/fclad" \
   --network host \
-  -h jackal \
-  --add-host jackal:127.0.0.1 \
-  --add-host jackal:192.168.8.100 \
-  --volume="/home/fclad/.vim:/root/.vim:ro" \
-  --volume="/home/fclad/.vimrc:/root/.vimrc:ro" \
-  --volume="/home/fclad/.config/nvim:/root/.config/nvim:ro" \
-  --volume="/samsungSSD:/samsungSSD" \
+  -h "$curr_dir" \
+  --add-host "$curr_dir:127.0.0.1" \
+  --volume="/home/fclad/.vim:/home/fclad/.vim:ro" \
+  --volume="/home/fclad/.vimrc:/home/fclad/.vimrc:ro" \
+  --volume="/home/fclad/.config/nvim:/home/fclad/.config/nvim:ro" \
+  --volume="/home/fclad/.bash_history:/home/fclad/.bash_history" \
+  --volume="/nvault:/nvault" \
   --rm \
   --security-opt seccomp=unconfined \
   --group-add=dialout \
