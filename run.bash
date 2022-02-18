9c264faee035390b77b863ed44e86fdc0cc02011
Fernando Cladera <fcladera@fcladera.com>
2022-02-18 18:20:45 -0500

    A single run.bash file to rule them all

--- run.bash
+++ run.bash
@@ -2,9 +2,25 @@
 
 # based on https://github.com/iandouglas96/jackal_master/blob/master/run.bash
 
-# Set the image name to the current dir
-curr_dir=${PWD##*/}
-IMG="fclad/$curr_dir"
+# Check that we have at least one param
+if [ $# -ne 1 ]
+  then
+    echo "Syntax: $0 docker_img"
+    exit 1
+fi
+
+# Set the image name to the argument of the script
+IMG="fclad/$1"
+
+# Check if the docker img exists locally
+if [[ "$(docker images -q $IMG 2> /dev/null)" == "" ]]; then
+    echo "Image $IMG does not exist locally. Is the name correct?"
+    exit 1
+fi
+
+# Set hostname by removing all dots from the first argument
+HOSTNAME=${1//./-}
+echo "Starting $IMG in hostname $HOSTNAME"
 
 # Make sure processes in the container can connect to the x server
 XAUTH=/tmp/.docker.xauth
@@ -28,18 +44,18 @@ docker run --gpus all \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
   -e XAUTHORITY=$XAUTH \
+  --network host \
   -v "/tmp/.X11-unix:/tmp/.X11-unix" \
   -v "/etc/localtime:/etc/localtime:ro" \
   -v "/dev:/dev" \
   -v "/media/$USER:/media/fclad" \
-  --network host \
-  -h "$curr_dir" \
-  --add-host "$curr_dir:127.0.0.1" \
-  --volume="/home/fclad/.vim:/home/fclad/.vim:ro" \
-  --volume="/home/fclad/.vimrc:/home/fclad/.vimrc:ro" \
+  -h "$HOSTNAME" \
+  --add-host "$HOSTNAME:127.0.0.1" \
   --volume="/home/fclad/.config/nvim:/home/fclad/.config/nvim:ro" \
   --volume="/home/fclad/.bash_history:/home/fclad/.bash_history" \
-  --volume="/nvault:/nvault" \
+  --volume="/home/fclad/.ssh:/home/fclad/.ssh" \
+  --volume="/nvault/GRASP/ROS_ws:/ROS_ws" \
+  --volume="/nvault/GRASP/bags:/bags" \
   --rm \
   --security-opt seccomp=unconfined \
   --group-add=dialout \
